33151d5fadf372ceba0fd5df561f9468ec5db096
Wagner Almeida <wdmeida@hotmail.com>
2015-07-04 22:40:37 -0300

    Atualização de telas

--- Filmoteca/src/tsi/lpv/samuelwagner/app/FilmotecaApp.java
+++ Filmoteca/src/tsi/lpv/samuelwagner/app/FilmotecaApp.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.sql.Connection;
 import java.util.Calendar;
 
+import tsi.lpv.samuelwagner.gui.IgCadastrarFilme;
+import tsi.lpv.samuelwagner.gui.IgFilmoteca;
 import tsi.lpv.samuelwagner.persistencia.ConnectionFactory;
 import tsi.lpv.samuelwagner.persistencia.FilmeDAO;
 import tsi.lpv.samuelwagner.tipo.Filme;
@@ -11,25 +13,7 @@ import tsi.lpv.samuelwagner.tipo.Filme;
 public class FilmotecaApp {
 
 	public static void main(String[] args) {
-		
-/*		FilmeDAO filme = new FilmeDAO();
-	Filme novo = new Filme();
-		novo.setTitulo("camisa");
-		novo.setAno(2000);
-		novo.setClassificacaoEtaria("Menor 10");
-		novo.setClassificacaoIMDB(9);
-		novo.setDataLancamento(Calendar.getInstance());
-		novo.setClassificacaoIMDB(2);
-		novo.setSinopse("eu sou a lenda");
-		novo.setDuracao(190);
-		novo.setMidia("DVD");
-		novo.setPoster(new File("C:\\Users\\Wagner\\Downloads\\gladiador.jpg"));
-		filme.cadastrarFilme(novo);
-		
-		//Cria um novo objeto e obtem o filme
-		Filme teste = filme.pesquisarFilme("camisa");
-		System.out.println(teste.getCodigo() + " " + teste.getTitulo());
-		System.out.println(filme.obterUltimoCodigo());*/
+		new IgFilmoteca();
 	}//testa
 
 }
--- Filmoteca/src/tsi/lpv/samuelwagner/gui/IgFilmoteca.java
+++ Filmoteca/src/tsi/lpv/samuelwagner/gui/IgFilmoteca.java
@@ -1,26 +1,129 @@
 package tsi.lpv.samuelwagner.gui;
 
+import java.awt.Color;
 import java.awt.HeadlessException;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+
 import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
 
 public class IgFilmoteca extends JFrame {
-
+	private Color corBase =  new Color(17, 17, 17);
+	private Color corFonte = new Color(237,255,40);
+	private Color corMenu = new Color(148,151,151);
+	private Color corSubMenu = new Color(124,61,139);
+	private JTextField pesquisarTextField;
+	
 	public IgFilmoteca() {
-		super("Filmoteca");
+		//Define o nome do app
+		super("Filmoteca Darth Vader");
+		
+		getContentPane().setBackground(corBase);
+		getContentPane().setForeground(corFonte);
+		getContentPane().setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(corBase);
+		panel.setBounds(0, 108, 153, 290);
+		getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JButton cadastrarFilmeButton = new JButton("Cadastrar Filme");
+		cadastrarFilmeButton.setIcon(new ImageIcon(IgFilmoteca.class.getResource("/tsi/lpv/samuelwagner/imagens/video.png")));
+		cadastrarFilmeButton.setBackground(Color.BLACK);
+		cadastrarFilmeButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		cadastrarFilmeButton.setForeground(Color.WHITE);
+		cadastrarFilmeButton.setToolTipText("Cadastrar Filmes");
+		cadastrarFilmeButton.setBounds(0, 11, 153, 38);
+		panel.add(cadastrarFilmeButton);
+		
+		JButton rankingButton = new JButton("Ver Preferidos");
+		rankingButton.setIcon(new ImageIcon(IgFilmoteca.class.getResource("/tsi/lpv/samuelwagner/imagens/keditbookmarks.png")));
+		rankingButton.setBackground(Color.BLACK);
+		rankingButton.setFont(new Font("Tahoma", Font.BOLD, 11));
+		rankingButton.setForeground(Color.WHITE);
+		rankingButton.setToolTipText("Visualizar filmes na ordem de prefer\u00EAncia.");
+		rankingButton.setBounds(0, 60, 153, 38);
+		panel.add(rankingButton);
+		
+		JButton btnGenro = new JButton("G\u00EAneros");
+		btnGenro.setBackground(Color.BLACK);
+		btnGenro.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnGenro.setForeground(Color.WHITE);
+		btnGenro.setToolTipText("Pesquisar por g\u00EAneros");
+		btnGenro.setBounds(0, 109, 153, 38);
+		panel.add(btnGenro);
+		
+		JPanel pesquisarPanel = new JPanel();
+		pesquisarPanel.setBackground(corSubMenu);
+		pesquisarPanel.setBounds(0, 21, 778, 38);
+		getContentPane().add(pesquisarPanel);
+		pesquisarPanel.setLayout(null);
+		
+		pesquisarTextField = new JTextField();
+		
+		//Registra o tratador de eventos do jtextField.
+		pesquisarTextField.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				pesquisarTextField.setText("");
+			}
+		});
+		
+		pesquisarTextField.setText("Pesquisar Filme");
+		pesquisarTextField.setToolTipText("Pesquisar Filme");
+		pesquisarTextField.setBounds(591, 9, 121, 20);
+		pesquisarPanel.add(pesquisarTextField);
+		pesquisarTextField.setColumns(10);
+		
+		JButton pesquisarButton = new JButton("");
+		pesquisarButton.setIcon(new ImageIcon(IgFilmoteca.class.getResource("/tsi/lpv/samuelwagner/imagens/Film_strip_24.png")));
+		pesquisarButton.setBackground(corSubMenu);
+		pesquisarButton.setBorderPainted(false);
+		pesquisarButton.setBounds(725, 4, 43, 30);
+		pesquisarPanel.add(pesquisarButton);
+		
+		//Define a tela como não redimensionável.
 		setResizable(false);
 		
-		setSize(720, 460);
+		setLocationByPlatform(true);
 		
+		//Define o tamanho da janela.
+		setSize(784, 492);
+		
+		//Define a ação a ser tomada quando o botão fechar for apertado.
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
-		JMenuBar menuBar = new JMenuBar();
-		setJMenuBar(menuBar);
+		JMenuBar principalMenuBar = new JMenuBar();
+		principalMenuBar.setBackground(corMenu);
+		setJMenuBar(principalMenuBar);
 		
+		JMenu arquivoMenu = new JMenu("Arquivo");
+		arquivoMenu.setBackground(corBase);
+		arquivoMenu.setForeground(Color.WHITE);
+		principalMenuBar.add(arquivoMenu);
+		
+		JMenuItem apagarMenuItem= new JMenuItem("Apagar banco de dados");
+		apagarMenuItem.setForeground(Color.WHITE);
+		apagarMenuItem.setBackground(corBase);
+		arquivoMenu.add(apagarMenuItem);
+		
+		//Define a tela como vísivel.
 		setVisible(true);
-	}
-}
+	}//IgFilmoteca
+}//class IgFilmoteca
