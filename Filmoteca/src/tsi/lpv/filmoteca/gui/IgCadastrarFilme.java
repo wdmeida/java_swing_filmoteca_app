c254ee11656e31eee3fafd8080f1ce2aadec7694
Wagner Almeida <wdmeida@gmail.com>
2016-01-28 18:53:14 -0200

    Atualização de Janelas

--- Filmoteca/src/tsi/lpv/filmoteca/gui/IgCadastrarFilme.java
+++ Filmoteca/src/tsi/lpv/filmoteca/gui/IgCadastrarFilme.java
@@ -2,7 +2,6 @@ package tsi.lpv.filmoteca.gui;
 
 import java.awt.CardLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
@@ -492,54 +491,31 @@ public class IgCadastrarFilme extends JDialog {
 		limparDiretorButton.setBounds(373, 335, 91, 23);
 		diretorPanel.add(limparDiretorButton);
 		
-		JPanel informacaoPanel = new JPanel();
-		informacaoPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informa\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
-		informacaoPanel.setBackground(corBase);
-		informacaoPanel.setBounds(228, 11, 249, 200);
-		diretorPanel.add(informacaoPanel);
-		informacaoPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		//Cria o painel com informações da aba.
+		JPanel informacoePanel = new JPanel();
+		informacoePanel.setForeground(Color.WHITE);
+		informacoePanel.setBackground(corBase);
+		informacoePanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informa\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
+		informacoePanel.setBounds(228, 11, 249, 100);
+		diretorPanel.add(informacoePanel);
+		informacoePanel.setLayout(null);
 		
-		JLabel lblInformeODiretores = new JLabel("Informe o Diretor(es) do Filme:");
-		lblInformeODiretores.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblInformeODiretores.setForeground(Color.WHITE);
-		informacaoPanel.add(lblInformeODiretores);
-
-		JLabel lblOCadastroDo = new JLabel("O cadastro do diretor não é obrigatório.");
-		lblOCadastroDo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblOCadastroDo.setForeground(Color.WHITE);
-		informacaoPanel.add(lblOCadastroDo);
-
-		JLabel lblComoCadastraO = new JLabel("Como cadastrar Diretor:");
-		lblComoCadastraO.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblComoCadastraO.setForeground(Color.WHITE);
-		informacaoPanel.add(lblComoCadastraO);
-
-		JLabel lblDigitaAO = new JLabel("Digite o nome do diretor na caixa de texto.");
-		lblDigitaAO.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDigitaAO.setForeground(Color.WHITE);
-		informacaoPanel.add(lblDigitaAO);
-
-	/*	JLabel lblDeDialogo = new JLabel("de Dialogo.");
-		lblDeDialogo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDeDialogo.setForeground(Color.WHITE);
-		informacaoPanel.add(lblDeDialogo);*/
-
-		JLabel lblAperteInserirOu = new JLabel("Aperte Inserir ou ALT+I.");
-		lblAperteInserirOu.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblAperteInserirOu.setForeground(Color.WHITE);
-		informacaoPanel.add(lblAperteInserirOu);
-
-		JLabel lblParaApagarO = new JLabel("Para apagar o último diretor.");
-		lblParaApagarO.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblParaApagarO.setForeground(Color.WHITE);
-		informacaoPanel.add(lblParaApagarO);
+		//TODO
 		
-		JLabel lblCliqueEmLipar = new JLabel("Clique em Limpar ou ALT+L");
-		lblCliqueEmLipar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblCliqueEmLipar.setForeground(Color.WHITE);
-		informacaoPanel.add(lblCliqueEmLipar);
+		//Cria o botão de ajuda da aba.
+		JButton ajudaButton = new JButton("Ajuda");
+		ajudaButton.setBounds(18, 35, 196, 20);
+		informacoePanel.add(ajudaButton);
+		ajudaButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new IgAjuda(IgCadastrarFilme.this, "Cadastrar Diretor", MensagensAjuda.CADASTRAR_DIRETOR);
+			}
+		});
+		
+		
 		TratadorEventoInsereDiretor eventoInsereDiretor = new TratadorEventoInsereDiretor(IgCadastrarFilme.this);
-		
 		inserirDiretorButton.setMnemonic(KeyEvent.VK_I);
 		limparDiretorButton.setMnemonic(KeyEvent.VK_L);
 		
@@ -605,53 +581,28 @@ public class IgCadastrarFilme extends JDialog {
 		inserirAutorButton.addActionListener(eventoInsereAutor);
 		limparAutorButton.addActionListener(eventoInsereAutor);
 		
-		JPanel informacaoPanel = new JPanel();
-		informacaoPanel.setForeground(Color.WHITE);
-		informacaoPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informa\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
-		informacaoPanel.setBackground(corBase);
-		informacaoPanel.setBounds(228, 11, 249, 200);
-		autorPanel.add(informacaoPanel);
-		informacaoPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		//Cria o painel com informações da aba.
+		JPanel informacoePanel = new JPanel();
+		informacoePanel.setForeground(Color.WHITE);
+		informacoePanel.setBackground(corBase);
+		informacoePanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informa\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
+		informacoePanel.setBounds(228, 11, 249, 100);
+		autorPanel.add(informacoePanel);
+		informacoePanel.setLayout(null);
 		
-		JLabel lblInformeOAutores = new JLabel("Informe o Autor(es) do Filme:");
-		lblInformeOAutores.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblInformeOAutores.setForeground(Color.WHITE);
-		informacaoPanel.add(lblInformeOAutores);
-
-		JLabel lblOCadastroDo_1 = new JLabel("O cadastro do autor não é obrigatorio.");
-		lblOCadastroDo_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblOCadastroDo_1.setForeground(Color.WHITE);
-		informacaoPanel.add(lblOCadastroDo_1);
-
-		JLabel lblComoCadastraO_1 = new JLabel("Como Cadastrar Autor:");
-		lblComoCadastraO_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblComoCadastraO_1.setForeground(Color.WHITE);
-		informacaoPanel.add(lblComoCadastraO_1);
-
-		JLabel lblDigitaAO_1 = new JLabel("Digite o nome do autor na caixa de texto.");
-		lblDigitaAO_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDigitaAO_1.setForeground(Color.WHITE);
-		informacaoPanel.add(lblDigitaAO_1);
-
-		JLabel lblDeAutor = new JLabel("de Autor.");
-		lblDeAutor.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDeAutor.setForeground(Color.WHITE);
-		informacaoPanel.add(lblDeAutor);
-
-		JLabel label_5 = new JLabel("Aperte Inserir ou ALT+I.");
-		label_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		label_5.setForeground(Color.WHITE);
-		informacaoPanel.add(label_5);
-
-		JLabel lblParaApagarO_1 = new JLabel("Para apagar o último autor.");
-		lblParaApagarO_1.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblParaApagarO_1.setForeground(Color.WHITE);
-		informacaoPanel.add(lblParaApagarO_1);
+		//TODO
 		
-		JLabel label = new JLabel("Clique em Limpar ou ALT+L");
-		label.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		label.setForeground(Color.WHITE);
-		informacaoPanel.add(label);
+		//Cria o botão de ajuda da aba.
+		JButton ajudaButton = new JButton("Ajuda");
+		ajudaButton.setBounds(18, 35, 196, 20);
+		informacoePanel.add(ajudaButton);
+		ajudaButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new IgAjuda(IgCadastrarFilme.this, "Cadastrar Autor", MensagensAjuda.CADASTRAR_AUTOR);
+			}
+		});
 		
 		erroAutorlabel = new JLabel("");
 		erroAutorlabel.setBounds(228, 222, 236, 23);
@@ -706,58 +657,26 @@ public class IgCadastrarFilme extends JDialog {
 		limparAtorButton.setBounds(373, 335, 91, 23);
 		artistaPanel.add(limparAtorButton);
 		
-		JPanel informacaoPanel = new JPanel();
-		informacaoPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informa\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
-		informacaoPanel.setBackground(corBase);
-		informacaoPanel.setBounds(228, 11, 249, 200);
-		artistaPanel.add(informacaoPanel);
+		//Cria o painel com informações da aba.
+		JPanel informacoePanel = new JPanel();
+		informacoePanel.setForeground(Color.WHITE);
+		informacoePanel.setBackground(corBase);
+		informacoePanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Informa\u00E7\u00F5es", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
+		informacoePanel.setBounds(228, 11, 249, 100);
+		artistaPanel.add(informacoePanel);
+		informacoePanel.setLayout(null);
 		
-		JLabel lblInformeOAtores = new JLabel("Informe o Ator(es) do Filme:");
-		lblInformeOAtores.setForeground(Color.WHITE);
-		lblInformeOAtores.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblInformeOAtores.setBounds(10, 25, 204, 21);
-		informacaoPanel.add(lblInformeOAtores);
-
-		JLabel lblOCadastroDo_2 = new JLabel("O cadastro do ator não é obrigatório.");
-		lblOCadastroDo_2.setForeground(Color.WHITE);
-		lblOCadastroDo_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblOCadastroDo_2.setBounds(20, 46, 204, 21);
-		informacaoPanel.add(lblOCadastroDo_2);
-
-		JLabel lblComoCadastraO_2 = new JLabel("Como Cadastrar Ator");
-		lblComoCadastraO_2.setForeground(Color.WHITE);
-		lblComoCadastraO_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblComoCadastraO_2.setBounds(10, 71, 204, 21);
-		informacaoPanel.add(lblComoCadastraO_2);
-
-		JLabel lblDigitaAO_2 = new JLabel("Digite o nome do ator na caixa de texto.");
-		lblDigitaAO_2.setForeground(Color.WHITE);
-		lblDigitaAO_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDigitaAO_2.setBounds(20, 92, 204, 21);
-		informacaoPanel.add(lblDigitaAO_2);
-
-		JLabel label_4 = new JLabel("de Dialogo.");
-		label_4.setForeground(Color.WHITE);
-		label_4.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		label_4.setBounds(20, 107, 194, 21);
-		informacaoPanel.add(label_4);
-
-		JLabel label_5 = new JLabel("Aperte Inserir ou ALT+I.");
-		label_5.setForeground(Color.WHITE);
-		label_5.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		label_5.setBounds(20, 124, 194, 29);
-		informacaoPanel.add(label_5);
-
-		JLabel lblParaApagarO_2 = new JLabel("Para apagar o último ator.");
-		lblParaApagarO_2.setForeground(Color.WHITE);
-		lblParaApagarO_2.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblParaApagarO_2.setBounds(20, 145, 194, 27);
-		informacaoPanel.add(lblParaApagarO_2);
-		
-		JLabel label = new JLabel("Clique em Limpar ou ALT+L");
-		label.setForeground(Color.WHITE);
-		label.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		informacaoPanel.add(label);
+		//Cria o botão de ajuda da aba.
+		JButton ajudaButton = new JButton("Ajuda");
+		ajudaButton.setBounds(18, 35, 196, 20);
+		informacoePanel.add(ajudaButton);
+		ajudaButton.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				new IgAjuda(IgCadastrarFilme.this, "Cadastrar Atores", MensagensAjuda.CADASTRAR_ATOR);
+			}
+		});
 		
 		erroAtorlabel = new JLabel("");
 		erroAtorlabel.setBounds(228, 222, 236, 23);
@@ -792,6 +711,7 @@ public class IgCadastrarFilme extends JDialog {
 		posterLabel.setBounds(10, 15, 208, 305);
 		posterImagemPanel.add(posterLabel);
 		
+		//Cria o painel com informações da aba.
 		JPanel informacoePanel = new JPanel();
 		informacoePanel.setForeground(Color.WHITE);
 		informacoePanel.setBackground(corBase);
@@ -800,6 +720,7 @@ public class IgCadastrarFilme extends JDialog {
 		posterPanel.add(informacoePanel);
 		informacoePanel.setLayout(null);
 		
+		//Cria o botão de ajuda da aba.
 		JButton ajudaButton = new JButton("Ajuda");
 		ajudaButton.setBounds(18, 35, 196, 20);
 		informacoePanel.add(ajudaButton);
@@ -807,59 +728,10 @@ public class IgCadastrarFilme extends JDialog {
 			
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				new IgAjuda(IgCadastrarFilme.this, "Cadastrar Postêr", MensagensAjuda.CADASTRAR_POSTER);
+				new IgAjuda(IgCadastrarFilme.this, "Cadastrar Pôster", MensagensAjuda.CADASTRAR_POSTER);
 			}
 		});
-		
-		
-		/*JLabel lblSelecioneOPster = new JLabel("Selecione o P\u00F4ster do filme:");
-		lblSelecioneOPster.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblSelecioneOPster.setForeground(Color.WHITE);
-		lblSelecioneOPster.setBounds(10, 22, 196, 20);
-		informacoePanel.add(lblSelecioneOPster);
-		
-		JLabel lblOPsteNo = new JLabel("O P\u00F4ster n\u00E3o é obrigatório.");
-		lblOPsteNo.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblOPsteNo.setForeground(Color.WHITE);
-		lblOPsteNo.setBounds(10, 47, 196, 20);
-		informacoePanel.add(lblOPsteNo);
-		
-		JLabel lblObrigatorio = new JLabel("obrigatório.");
-		lblObrigatorio.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblObrigatorio.setForeground(Color.WHITE);
-		lblObrigatorio.setBounds(10, 64, 70, 20);
-		informacoePanel.add(lblObrigatorio);
-		
-		JLabel lblComoSelecionarO = new JLabel("Como selecionar o P\u00F4ster:");
-		lblComoSelecionarO.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblComoSelecionarO.setForeground(Color.WHITE);
-		lblComoSelecionarO.setBounds(10, 95, 196, 20);
-		informacoePanel.add(lblComoSelecionarO);
-		
-		JLabel lblCliqueNoBoto = new JLabel("Clique no bot\u00E3o Inserir ou utilize");
-		lblCliqueNoBoto.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblCliqueNoBoto.setForeground(Color.WHITE);
-		lblCliqueNoBoto.setBounds(20, 114, 199, 20);
-		informacoePanel.add(lblCliqueNoBoto);
-		
-		JLabel lblOAlti = new JLabel(" o ALT+I.");
-		lblOAlti.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblOAlti.setForeground(Color.WHITE);
-		lblOAlti.setBounds(20, 132, 186, 20);
-		informacoePanel.add(lblOAlti);
-		
-		JLabel lblDepoisESelecionar = new JLabel("Depois e selecionar a");
-		lblDepoisESelecionar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblDepoisESelecionar.setForeground(Color.WHITE);
-		lblDepoisESelecionar.setBounds(20, 159, 186, 20);
-		informacoePanel.add(lblDepoisESelecionar);
-		
-		JLabel lblImagemEConfimar = new JLabel("Imagem e confimar");
-		lblImagemEConfimar.setFont(new Font("Tahoma", Font.PLAIN, 14));
-		lblImagemEConfimar.setForeground(Color.WHITE);
-		lblImagemEConfimar.setBounds(20, 181, 186, 20);
-		informacoePanel.add(lblImagemEConfimar);*/
-		
+				
 		JPanel fotoPanel = new JPanel();
 		fotoPanel.setBackground(corBase);
 		fotoPanel.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"), "Foto", TitledBorder.LEADING, TitledBorder.TOP, null, Color.WHITE));
--- Filmoteca/src/tsi/lpv/filmoteca/mensagens/MensagensAjuda.java
+++ Filmoteca/src/tsi/lpv/filmoteca/mensagens/MensagensAjuda.java
@@ -8,4 +8,19 @@ public interface MensagensAjuda {
 			+ " queira escolher outra imagem, basta clicar no botão LIMPAR e realizar novamente a escolha "
 			+ "da imagem."
 			+ "\n\nO cadastro do Pôster não é obrigatório.";
+	
+	public String CADASTRAR_ATOR = "Caso queira cadastrar um ator,"
+			+ " digite o nome do mesmo na caixa de texto e clique no botão INSERIR ou utilize as teclas de atalho ALT + I."
+			+ " Podem ser cadastrados vários atores, um de cada vez. Caso queira remover o último ator cadastrado, clique"
+			+ " no botão LIMPAR ou utilize a tecla de atalho ALT + L.";
+	
+	public String CADASTRAR_AUTOR = "Caso queira cadastrar um autor,"
+			+ " digite o nome do mesmo na caixa de texto e clique no botão INSERIR ou utilize as teclas de atalho ALT + I."
+			+ " Podem ser cadastrados vários autores, um de cada vez. Caso queira remover o último autor cadastrado, clique"
+			+ " no botão LIMPAR ou utilize a tecla de atalho ALT + L.";
+	
+	public String CADASTRAR_DIRETOR = "Caso queira cadastrar um diretor,"
+			+ " digite o nome do mesmo na caixa de texto e clique no botão INSERIR ou utilize as teclas de atalho ALT + I."
+			+ " Podem ser cadastrados vários diretores, um de cada vez. Caso queira remover o último diretor cadastrado, clique"
+			+ " no botão LIMPAR ou utilize a tecla de atalho ALT + L.";
 }//interface MensagensAjuda
